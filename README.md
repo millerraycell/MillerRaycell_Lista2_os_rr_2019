676d832d105b9005adee7827654a8e739aba732a
Miller Monteiro <millerraycell@gmail.com>
2019-06-12 19:57:13 -0400

    Atualização README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MillerRaycell_Lista2_os_rr_2019
 Repositório com as respostas da lista 2 da disciplina de Sistemas Operacionais da Universidade Federal de Roraima
+
+		Para compilar os códigos basta abrir um terminal no repositório e digitar ./rodar_programas, atraves desse comando pode se observar a execução dos códigos
+
+		A quarta questão é relativa a subida de um módulo para o kernel, logo quando for rodar os códigos recomendo ter todos os arquivos salvos, o código foi desenvolvido para não dar problema no seu kernel, but better safe then sorry.
+
+		Muito obrigado :)
--- rodar_programas.sh
+++ rodar_programas.sh
@@ -8,5 +8,25 @@ cd Questao_3
 g++ numeros_primos.cpp -o numeros_primos -pthread
 ./numeros_primos
 
-echo "Salvando saidas em um arquivo"
-./numeros_primos > saidas.txt
+echo "SAIDAS PODEM SER ANALISADAS NO ARQUIVO SAIDAS.TXT NA PASTA DA QUESTAO"
+
+echo ""
+echo ""
+
+echo "QUESTAO 4"
+
+cd ..
+cd Questao_4
+
+echo "FAZENDO O MAKE DO MODULO"
+make
+
+echo "SUBINDO O MODULO PARA O KERNEL"
+sudo insmod listaTarefas.ko
+
+echo "RETIRANDO O MODULO PARA O KERNEL"
+sudo rmmod listaTarefas.ko
+
+echo "LOG DO KERNEL DEMOSNTRANDO A LISTA FUNCIONANDO"
+dmesg
+echo "SAIDAS PODEM SER ANALISADAS NO ARQUIVO SAIDAS.TXT NA PASTA DA QUESTAO"
